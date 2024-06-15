89ee56747d57143d7b36276b33b0738d345f47d2
hyeokls <hyeoknix@gmail.com>
2024-06-16 00:07:07 +0900

    Update Generate for-loop: 100-bit binary adder 2(Adder100i).v

--- More Verilog Features/Generate for-loop: 100-bit binary adder 2(Adder100i).v
+++ More Verilog Features/Generate for-loop: 100-bit binary adder 2(Adder100i).v
@@ -7,7 +7,8 @@ module top_module(
     integer i;
     always@(*)begin
         sum[0]= a[0] ^ b[0] ^ cin;
-        cout[0] = a[0] & b[0] | cin & (a[0]|b[0]);
+	    cout[0] = (a[0] & b[0]) | (b[i] & cin) | (cin & a[i])
+       // cout[0] = a[0] & b[0] | cin & (a[0]|b[0]);
         
         for(i =1; i<100; i++)begin
             sum[i] = a[i] ^b[i] ^cout[i-1];
