fcd195c6ddfd278f3098dc68e0d94042242452d2
hyeokls <hyeoknix@gmail.com>
2024-06-17 21:21:44 +0900

    Update 05.256-to-1 4bit multipleexer(Mux256to1v).v

--- Circuits/Combinational Logic/Multiplexers/05.256-to-1 4bit multipleexer(Mux256to1v).v
+++ Circuits/Combinational Logic/Multiplexers/05.256-to-1 4bit multipleexer(Mux256to1v).v
@@ -1,1 +1,19 @@
+module top_module( 
+    input [1023:0] in,
+    input [7:0] sel,
+    output [3:0] out );
+    
+    integer i;
+    always@(*)begin
+    out=0;
+        for(i=0; i<1024; i=i+4)begin
+            if(sel== i/4)begin
+                out= in[i+:4];
+            end			
+        end
+  
+    end
+    
+endmodule
 
+//assign out = {in[sel*4+3], in[sel*4+2], in[sel*4+1], in[sel*4+0]};
