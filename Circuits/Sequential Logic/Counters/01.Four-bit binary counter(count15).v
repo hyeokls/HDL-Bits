23adccaf7403c386c193443959696f026ee010eb
hyeokls <hyeoknix@gmail.com>
2024-06-22 10:28:46 +0900

    Update 01.Four-bit binary counter(count15).v

--- Circuits/Sequential Logic/Counters/01.Four-bit binary counter(count15).v
+++ Circuits/Sequential Logic/Counters/01.Four-bit binary counter(count15).v
@@ -1,7 +1,7 @@
 module top_module (
     input clk,
     input reset,      // Synchronous activea-high reset
-    output [3:0] q);
+    output reg[3:0] q);
     wire[3:0] qin;
     always@(posedge clk)begin
         if(reset==1)begin
@@ -16,3 +16,13 @@ module top_module (
     end
     
 endmodule
+
+// Because q is 4 bits, it rolls over from 15 -> 0.
+// If you want a counter that counts a range different from 0 to (2^n)-1, 
+// then you need to add another rule to reset q to 0 when roll-over should occur.
+
+always @(posedge clk)
+		if (reset)
+			q <= 0;
+		else
+			q <= q+1;	
